e5aa295aa33d38f82195a5f3daafb206de1bfca2
Jeremy Tupas <jeremytupas@gmail.com>
2022-09-27 09:17:19 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Jeremy Tupas | Web Developer Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
